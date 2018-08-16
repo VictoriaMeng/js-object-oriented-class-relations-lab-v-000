e5e725069b0f56f611eafc80cd240b85e4dd5f97
Victoria Meng <nights1ststar@protonmail.com>
2018-08-16 21:52:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const store = { drivers: [], passengers: [] };
 let driverId = 0
+
 class Driver {
   constructor(name) {
     this.name = name;
     store.drivers.push(this);
+    this.id = ++driverId;
   };
 };
