3e34c6750bfec755028dfbff51b345bfe3841e51
Victoria Meng <nights1ststar@protonmail.com>
2018-08-16 22:03:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,10 +25,6 @@ class Trip {
     this.id = ++tripId;
     this.driverId = driver.id;
     this.passengerId = passenger.id;
-
-    driver() {
-      return store.drivers.find(function(driver) {return driver.id = this.driverId});
-    };
   };
 
 
